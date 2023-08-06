62d9111b89a0163d2c56774ad8413b563159e89c
asenkits <140309006+asenkits@users.noreply.github.com>
2023-08-06 13:04:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -317,7 +317,7 @@ const BLOG = {
       process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
 
   SEO_BAIDU_SITE_VERIFICATION:
-      process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '<BLOG.SEO_BAIDU_SITE_VERIFICATION>', // Remove the value or replace it with your own google site verification code
+      process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
 
   // <---- 站点统计
 
