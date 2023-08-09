1c9eff071db3b9ff0470fc5b1c67e70dc4f2d42a
asenkits <140309006+asenkits@users.noreply.github.com>
2023-08-09 23:33:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -317,8 +317,8 @@ const BLOG = {
       process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
 
   SEO_BAIDU_SITE_VERIFICATION:
-      process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
-  meta name="baidu-site-verification" content="codeva-hxbI45M20e"
+      process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || 'codeva-hxbI45M20e', // Remove the value or replace it with your own google site verification code
+
   // <---- 站点统计
 
   // 谷歌广告
